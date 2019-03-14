57e1118b1c2d1306ef1d479ab9e917d281870eaa
Benny V <bengv2303@gmail.com>
2019-03-14 13:56:49 -0600

    Updates

---
+++ precache-manifest.fd90bbd02b1b7e4c9c1909093f71def2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "569b2b33e303e431ba73",
+    "url": "/myapp/static/js/runtime~main.778762fe.js"
+  },
+  {
+    "revision": "82478a3547ef70ada779",
+    "url": "/myapp/static/js/main.eee0397b.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/myapp/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "82478a3547ef70ada779",
+    "url": "/myapp/static/css/main.09295e4e.chunk.css"
+  },
+  {
+    "revision": "707e52dff69e808188c23ab64579f505",
+    "url": "/myapp/index.html"
+  }
+];
