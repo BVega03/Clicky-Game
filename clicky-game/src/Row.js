fd3e7f96f7adf953613fd68ba2df856281142acd
Ben Vega <bengv2303@gmail.com>
2019-03-28 19:46:17 -0600

    package.json update, add images

--- clicky-game/src/Row.js
+++ clicky-game/src/Row.js
@@ -6,10 +6,3 @@ const Row = props =>
   </div>;
 
 export default Row;
-
-
-// {
-//     "id": 1,
-//     "image": "http://www.simpsoncrazy.com/content/pictures/regulars/ComicBookGuy2.gif",
-//     "clicked": false
-//   },
